f982251088ae93967282c9ba0949b073b9400330
CSavi <carrielane89@gmail.com>
2018-11-15 16:03:46 -0500

    finish /api/listing controller actions

---
+++ app/controllers/api/listings_controller.rb
@@ -0,0 +1,49 @@
+class Api::ListingsController < ApplicationController 
+    before_action :set_listing, only: [:show, :update, :destroy]
+
+    def index
+        render json: Listing.all
+    end
+
+    def create 
+        listing = Listing.new(listing_params)
+        if listing.save
+            render json: listing
+        else 
+            render json: { message: listing.errors }, status: 400
+        end 
+    end 
+
+
+    def show 
+        render json: @listing
+    end 
+
+    def update 
+        if @listing.update(listing_params)
+            render json: @listing 
+        else 
+            render json: { message: @listing.errors }, status: 400
+        end 
+    end 
+
+    def destroy 
+        if @listing.destroy
+            render status: 204 
+        else 
+            render json: { message: "Unable to remove this listing"}, status: 400
+        end 
+    end 
+
+    private 
+
+        def listing_params
+            params.require(:listing).permit(:name, :price, :img_url, :street, :city, :state, :postal_code, :description, :host, :host_id)
+        end
+
+        # helper method
+        def set_listing
+            @listing = Listing.find_by(id: params[:id])
+        end 
+end
+
