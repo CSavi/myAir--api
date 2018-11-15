86f16bfeac171a4072296bb1976aa7606bc45ac0
CSavi <carrielane89@gmail.com>
2018-11-15 15:39:16 -0500

    Add /api/listings resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :listings, excpet: [:new, :edit]
+  end 
 end
+
+
+# GET /api/listings
+# GET /api/listings/:id
+# POST /api/listings
+# PUT /api/listings/:id (update/edit)
+# DELETE /api/listings/:id
